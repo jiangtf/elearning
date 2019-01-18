14d3665851a1be4a0a41cfa31db85e7269c97c27
zween <26199802@163.com>
2019-01-18 17:29:31 +0800

    选择题目页面切换优化

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,13 @@
 $(function () {
+    //按钮状态
+    $(document).on('click', '.buttons-row .button', function () {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    });
+    //解析答案
+    $(document).on('click', '.resolve', function (e) {
+        $(this).parents('.list-block').next('.card').removeClass('hidden');
+    });
 
     $.init();    //默认必须要执行$.init(),实际业务里一般不会在HTML文档里执行，通常是在业务页面代码的最后执行
     $(".swiper-container").swiper({
